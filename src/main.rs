138ddb0b91a5712ff1e30a58d29e4cad664ea679
Matt Jones <jonesmr@gmail.com>
2018-10-22 01:36:44 +0000

    Generate image using image/imageproc, and render that

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+extern crate image;
+extern crate imageproc;
 extern crate rppal;
+extern crate rusttype;
 
 use rppal::gpio::{Gpio, Level, Mode};
 use rppal::spi::{Spi};
 
-const EPD_WIDTH: u32 = 640;
-const EPD_HEIGHT: u32 = 384;
+const EPD_WIDTH: usize = 640;
+const EPD_HEIGHT: usize = 384;
 
 const RST_PIN : u8 = 17;
 const DC_PIN : u8 = 25;
@@ -47,21 +50,7 @@ fn wait_until_idle(gpio: &mut Gpio) {
     }
 }
 
-fn main() {
-    println!("Hello, world. Yo!");
-
-    std::thread::sleep(std::time::Duration::from_millis(1000));
-
-    let mut gpio = rppal::gpio::Gpio::new().expect("Gpio::new()");
-
-    // Don't forget to enable SPI with sudo raspi-config
-    let mut spi = rppal::spi::Spi::new(
-        rppal::spi::Bus::Spi0,
-        rppal::spi::SlaveSelect::Ss0,
-        2000000,
-        rppal::spi::Mode::Mode0).expect("Spi::new()");
-
-    // INIT
+fn init_display(gpio: &mut Gpio, spi: &mut Spi) {
 
     gpio.set_mode(RST_PIN, Mode::Output);
     gpio.set_mode(DC_PIN, Mode::Output);
@@ -73,53 +62,116 @@ fn main() {
     gpio.write(RST_PIN, Level::High);
     std::thread::sleep(std::time::Duration::from_millis(200));
 
-    send_command(&mut gpio, &mut spi, POWER_SETTING);
-    send_data(&mut gpio, &mut spi, 0x37);
-    send_data(&mut gpio, &mut spi, 0x00);
+    send_command(gpio, spi, POWER_SETTING);
+    send_data(gpio, spi, 0x37);
+    send_data(gpio, spi, 0x00);
 
-    send_command(&mut gpio, &mut spi, PANEL_SETTING);
-    send_data(&mut gpio, &mut spi, 0xCF);
-    send_data(&mut gpio, &mut spi, 0x08);
+    send_command(gpio, spi, PANEL_SETTING);
+    send_data(gpio, spi, 0xCF);
+    send_data(gpio, spi, 0x08);
 
-    send_command(&mut gpio, &mut spi, BOOSTER_SOFT_START);
-    send_data(&mut gpio, &mut spi, 0xc7);
-    send_data(&mut gpio, &mut spi, 0xcc);
-    send_data(&mut gpio, &mut spi, 0x28);
+    send_command(gpio, spi, BOOSTER_SOFT_START);
+    send_data(gpio, spi, 0xc7);
+    send_data(gpio, spi, 0xcc);
+    send_data(gpio, spi, 0x28);
 
-    send_command(&mut gpio, &mut spi, POWER_ON);
-    wait_until_idle(&mut gpio);
+    send_command(gpio, spi, POWER_ON);
+    wait_until_idle(gpio);
 
-    send_command(&mut gpio, &mut spi, PLL_CONTROL);
-    send_data(&mut gpio, &mut spi, 0x3c);
+    send_command(gpio, spi, PLL_CONTROL);
+    send_data(gpio, spi, 0x3c);
 
-    send_command(&mut gpio, &mut spi, TEMPERATURE_CALIBRATION);
-    send_data(&mut gpio, &mut spi, 0x00);
+    send_command(gpio, spi, TEMPERATURE_CALIBRATION);
+    send_data(gpio, spi, 0x00);
 
-    send_command(&mut gpio, &mut spi, VCOM_AND_DATA_INTERVAL_SETTING);
-    send_data(&mut gpio, &mut spi, 0x77);
+    send_command(gpio, spi, VCOM_AND_DATA_INTERVAL_SETTING);
+    send_data(gpio, spi, 0x77);
 
-    send_command(&mut gpio, &mut spi, TCON_SETTING);
-    send_data(&mut gpio, &mut spi, 0x22);
+    send_command(gpio, spi, TCON_SETTING);
+    send_data(gpio, spi, 0x22);
 
-    send_command(&mut gpio, &mut spi, TCON_RESOLUTION);
-    send_data(&mut gpio, &mut spi, 0x02);     //source 640
-    send_data(&mut gpio, &mut spi, 0x80);
-    send_data(&mut gpio, &mut spi, 0x01);     //gate 384
-    send_data(&mut gpio, &mut spi, 0x80);
+    send_command(gpio, spi, TCON_RESOLUTION);
+    send_data(gpio, spi, 0x02);     //source 640
+    send_data(gpio, spi, 0x80);
+    send_data(gpio, spi, 0x01);     //gate 384
+    send_data(gpio, spi, 0x80);
 
-    send_command(&mut gpio, &mut spi, VCM_DC_SETTING);
-    send_data(&mut gpio, &mut spi, 0x1E);      //decide by LUT file;
+    send_command(gpio, spi, VCM_DC_SETTING);
+    send_data(gpio, spi, 0x1E);      //decide by LUT file;
 
-    send_command(&mut gpio, &mut spi, 0xe5);           //FLASH MODE;
-    send_data(&mut gpio, &mut spi, 0x03);
+    send_command(gpio, spi, 0xe5);           //FLASH MODE;
+    send_data(gpio, spi, 0x03);
 
     // Draw all black?
-    send_command(&mut gpio, &mut spi, DATA_START_TRANSMISSION);
+    send_command(gpio, spi, DATA_START_TRANSMISSION);
+}
 
-    for i in 0..122880 {
-        send_data(&mut gpio, &mut spi, 0x00);
+fn display_image(gpio: &mut Gpio, spi: &mut Spi, imgbuf: &image::ImageBuffer<image::Luma<u8>, Vec<u8>>) {
+    let mut pixel_in_progress: u8 = 0;
+
+    use image::Pixel;
+
+    for y in 0..EPD_HEIGHT {
+        for x in 0..EPD_WIDTH {
+            let color = imgbuf.get_pixel(x as u32, y as u32).to_luma().data[0];
+            if x % 2 == 0 {
+                if color < 64{
+                    pixel_in_progress = 0x00;
+                } else if color < 128 {
+                    pixel_in_progress = 0x10;
+                } else if color < 192 {
+                    pixel_in_progress = 0x20;
+                } else {
+                    pixel_in_progress = 0x30;
+                }
+            } else {
+                if color < 64 {
+                    pixel_in_progress |= 0x00;
+                } else if color < 128 {
+                    pixel_in_progress |= 0x01;
+                } else if color < 192 {
+                    pixel_in_progress |= 0x02;
+                } else {
+                    pixel_in_progress |= 0x03;
+                }
+
+                send_data(gpio, spi, pixel_in_progress);
+            }
+        }
     }
-    send_command(&mut gpio, &mut spi, DISPLAY_REFRESH);
+
+    send_command(gpio, spi, DISPLAY_REFRESH);
     std::thread::sleep(std::time::Duration::from_millis(100));
-    wait_until_idle(&mut gpio);
+    wait_until_idle(gpio);
+
+}
+
+fn main() {
+    let args: Vec<_> = std::env::args().collect();
+    let display = args.len() == 1 || args[1] != "nopi";
+    println!("Running. display={}", display);
+
+    let mut imgbuf = image::GrayImage::new(EPD_WIDTH as u32, EPD_HEIGHT as u32);
+    let font = Vec::from(include_bytes!("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf") as &[u8]);
+    let font = rusttype::FontCollection::from_bytes(font).unwrap().into_font().unwrap();
+
+    let scale = rusttype::Scale { x: 50.0, y: 50.0 };
+    imageproc::drawing::draw_filled_rect_mut(&mut imgbuf, imageproc::rect::Rect::at(0,0).of_size(EPD_WIDTH as u32, EPD_HEIGHT as u32), image::Luma{data: [255u8; 1]});
+    imageproc::drawing::draw_text_mut(&mut imgbuf, image::Luma{data: [0u8; 1]}, 10, 10, scale, &font, "Cristina is pretty");
+
+    let _ = imgbuf.save("/tmp/image.png").unwrap();
+
+    if display {
+        let mut gpio = rppal::gpio::Gpio::new().expect("Gpio::new()");
+
+        // Don't forget to enable SPI with sudo raspi-config
+        let mut spi = rppal::spi::Spi::new(
+            rppal::spi::Bus::Spi0,
+            rppal::spi::SlaveSelect::Ss0,
+            2000000,
+            rppal::spi::Mode::Mode0).expect("Spi::new()");
+
+        init_display(&mut gpio, &mut spi);
+        display_image(&mut gpio, &mut spi, &imgbuf);
+    }
 }
