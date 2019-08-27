293f921c8a03a9285106f31c0da68aa958facde9
Matt Jones <jonesmr@gmail.com>
2019-08-27 19:34:14 +0000

    remove obsolete genproto

--- genproto.sh
+++
@@ -1,6 +0,0 @@
-# 1. Install generic, Google protoc:
-# - Best is to get a new version from: https://github.com/google/protobuf/releases
-# - You can also do: sudo apt-get install protobuf-compiler  (But this is pretty old).
-# 2. Install rust protoc plugin:
-# - cargo install protobuf
-~/downloads/proto/bin/protoc --proto_path ./proto/ --plugin ~/.cargo/bin/protoc-gen-rust --rust_out ./src/ proto/*.proto
